a7aed4cc9678d8a1af54aa9c716dedd01265145b
Julien Blondeau <julienblondeau@live.fr>
2021-03-25 17:41:07 +0100

    add password field to user model

--- models/user.js
+++ models/user.js
@@ -14,22 +14,21 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   User.init({
-    id: {
-      type: DataTypes.INTEGER(11),
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true
-    },
     username: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(20),
       allowNull: false,
       unique: true
     },
     email: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(100),
       allowNull: false,
       unique: true
-    }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    imageUrl: DataTypes.String
   }, {
     sequelize,
     modelName: 'User',
